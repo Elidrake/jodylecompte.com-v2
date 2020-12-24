4ecd2de96b2e5af8a14a91b255cb790f952ff161
Jody LeCompte <jody@jodylecompte.com>
2020-12-24 13:46:51 -0600

    Implement table of contents

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -7,13 +7,18 @@ export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
   const { markdownRemark } = data // data.markdownRemark holds your post data
-  const { frontmatter, html } = markdownRemark
+  const { frontmatter, html, tableOfContents } = markdownRemark
   return (
     <Layout>
       <div className="blog-post-container">
         <div className="blog-post">
           <h1>{frontmatter.title}</h1>
-          <h2>{frontmatter.date}</h2>
+          <div className="blog-posted-date">Posted on {frontmatter.date}</div>
+          <div className="blog-toc">
+            <h2>Table of Contents</h2>
+            <div dangerouslySetInnerHTML={{ __html: tableOfContents }}></div>
+          </div>
+
           <div
             className="blog-post-content"
             dangerouslySetInnerHTML={{ __html: html }}
@@ -27,6 +32,11 @@ export const pageQuery = graphql`
   query($slug: String!) {
     markdownRemark(frontmatter: { slug: { eq: $slug } }) {
       html
+      tableOfContents(
+        absolute: false
+        pathToSlugField: "frontmatter.slug"
+        maxDepth: 3
+      )
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
         slug
